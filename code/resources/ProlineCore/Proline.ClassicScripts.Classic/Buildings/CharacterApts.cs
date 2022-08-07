4d6f5cecf874f51a7d4f6960f6700af7226d5a36
Wayne-Thornberry <way-net@outlook.com>
2022-08-08 00:46:49 +0100

    multiple apt support

--- code/resources/ProlineCore/Proline.ClassicScripts.Classic/Buildings/CharacterApts.cs
+++ code/resources/ProlineCore/Proline.ClassicScripts.Classic/Buildings/CharacterApts.cs
@@ -1,4 +1,5 @@
 ﻿using CitizenFX.Core;
+using CitizenFX.Core.Native;
 using Newtonsoft.Json;
 using Proline.ClassicOnline.MWorld;
 using System;
@@ -11,42 +12,40 @@ using System.Threading.Tasks;
 namespace Proline.ClassicOnline.SClassic.Buildings
 {
     public class CharacterApts
-    {
-        private const string PropertyId = "apt_richmaj_he_01";
+    { 
         private List<Vector3> _buildingEntrances;
         private List<Vector3> _interiorExits;
         private Blip _blip;
-        private string _targetProperty;
+        private string _targetPropertyPart;
         private string _targetArea;
-        private Vector3 _lastPoint; 
-        private string _t;
+        private Vector3 _lastPoint;  
         private Vector3 _buildingVector;
         private string _interior;
         private string _enteredBuilding;
         private string _neariestEntrance;
+        private string _targetProperty;
 
         public async Task Execute(object[] args, CancellationToken token)
         {
             // Dupe protection
             if (MScripting.MScriptingAPI.GetInstanceCountOfScript("CharacterApts") > 1)
                 return;
-
-            _t = "apt";
-            //RefreshExitPoints();
-            RefreshEntryPoints();
-
-            _blip = World.CreateBlip(new Vector3(-1443.171f, -544.501f, 34.74184f));
-            _blip.Sprite = BlipSprite.GTAOPlayerSafehouse;
-            var stage = 0;
-
-
-
+            var properties = new string[] { "apt_richmaj_he_01", "apt_dpheights_he_01" };
+            var stage = 0; 
             while (!token.IsCancellationRequested)
             {
 
                 switch (stage)
                 {
                     case 0:
+                        _buildingEntrances = new List<Vector3>();
+                        foreach (var item in properties)
+                        {
+                            RefreshEntryPoints(item);
+                        }
+                        stage++;
+                        break; 
+                    case 1: 
                         foreach (var entrance in _buildingEntrances)
                         { 
                             World.DrawMarker(MarkerType.DebugSphere, entrance, new Vector3(0, 0, 0), new Vector3(0, 0, 0), new Vector3(1, 1, 1),
@@ -56,34 +55,36 @@ namespace Proline.ClassicOnline.SClassic.Buildings
                                 _enteredBuilding = WorldAPI.GetNearestBuilding();
                                 _neariestEntrance = WorldAPI.GetNearestBuildingEntrance(_enteredBuilding);
                                 _buildingVector = WorldAPI.GetBuildingWorldPos(_enteredBuilding);  
-                                var whereAreYouEntering = WorldAPI.EnterBuilding(_enteredBuilding, _neariestEntrance); 
+                                var whereAreYouEntering = WorldAPI.EnterBuilding(_enteredBuilding, _neariestEntrance);
+                                _targetProperty = "apt_richmaj_he_01";
                                 switch (whereAreYouEntering)
                                 {
-                                    case "Garage": _targetProperty = WorldAPI.GetPropertyGarage(PropertyId);  break;
-                                    case "Apartment": _targetProperty = WorldAPI.GetPropertyApartment(PropertyId); break;
+                                    case "Garage": _targetPropertyPart = WorldAPI.GetPropertyGarage(_targetProperty);  break;
+                                    case "Apartment": _targetPropertyPart = WorldAPI.GetPropertyApartment(_targetProperty); break;
                                 }
                                 _targetArea = whereAreYouEntering;
                                 //WorldAPI.EnterProperty(PropertyId, _targetProperty, _neariestEntrance);
-                                _interior = WorldAPI.GetPropertyInterior(_targetProperty, whereAreYouEntering + "s");
-                                var interiorEntry = WorldAPI.GetPropertyEntry(_targetProperty, whereAreYouEntering + "s", _neariestEntrance);
+                                _interior = WorldAPI.GetPropertyInterior(_targetPropertyPart, whereAreYouEntering + "s");
+                                var interiorEntry = WorldAPI.GetPropertyEntry(_targetPropertyPart, whereAreYouEntering + "s", _neariestEntrance);
                                 _lastPoint = WorldAPI.EnterInterior(_interior, interiorEntry);
                                 Game.PlayerPed.Position = _lastPoint; 
                                 RefreshExitPoints();
-                                stage = 1;
-                                MDebug.MDebugAPI.LogDebug(stage);
+                                stage++; 
                             }
                         } 
                         break;
-                    case 1:
+                    case 2:
                         {
                             if (World.GetDistance(Game.PlayerPed.Position, _lastPoint) > 4f)
                             { 
-                                stage = 2;
-                                MDebug.MDebugAPI.LogDebug(stage);
+                                stage++; 
                             }
                         }
                         break;
-                    case 2:
+                    case 3:
+                        if (API.GetInteriorFromEntity(Game.PlayerPed.Handle) == 0)
+                            stage = 0;
+
                         foreach (var exit in _interiorExits)
                         { 
                             World.DrawMarker(MarkerType.DebugSphere, exit, new Vector3(0, 0, 0), new Vector3(0, 0, 0), new Vector3(1, 1, 1),
@@ -92,17 +93,15 @@ namespace Proline.ClassicOnline.SClassic.Buildings
                             { 
                                 var neariestExit = WorldAPI.GetNearestInteriorExit(_interior);
                                 var whereAreYouExiting = WorldAPI.ExitInterior(_interior, neariestExit); 
-                                var newExit = WorldAPI.GetPropertyExit(_targetProperty, _targetArea + "s", neariestExit);
+                                var newExit = WorldAPI.GetPropertyExit(_targetPropertyPart, _targetArea + "s", neariestExit);
                                 var placePosition = WorldAPI.ExitBuilding(_enteredBuilding, newExit, Game.PlayerPed.IsInVehicle() ? 1 : 0);
                                 Game.PlayerPed.Position = placePosition;
-                                _lastPoint = placePosition;
-                                RefreshEntryPoints(); 
-                                stage = 3;
-                                MDebug.MDebugAPI.LogDebug(stage);
+                                _lastPoint = placePosition; 
+                                stage++; 
                             }
                         } 
                         break;
-                    case 3:
+                    case 4:
                         {
                             if (World.GetDistance(Game.PlayerPed.Position, _lastPoint) > 4f)
                             {
@@ -117,22 +116,20 @@ namespace Proline.ClassicOnline.SClassic.Buildings
             }
         }
 
-        private void RefreshEntryPoints()
+        private void RefreshEntryPoints(string propertyId)
         {
-            _buildingEntrances = new List<Vector3>();
-            for (int i = 0; i < MWorld.WorldAPI.GetNumOfBuldingEntrances(WorldAPI.GetPropertyBuilding(PropertyId)); i++)
+            for (int i = 0; i < WorldAPI.GetNumOfBuldingEntrances(WorldAPI.GetPropertyBuilding(propertyId)); i++)
             {
-                _buildingEntrances.Add(MWorld.WorldAPI.GetBuildingEntrance(WorldAPI.GetPropertyBuilding(PropertyId), i));
+                _buildingEntrances.Add(WorldAPI.GetBuildingEntrance(WorldAPI.GetPropertyBuilding(propertyId), i));
             };
         }
 
         private void RefreshExitPoints()
-        {
-            var id = _t.Equals("gar") ? 0 : 1;
+        { 
             _interiorExits = new List<Vector3>(); 
-            for (int i = 0; i < MWorld.WorldAPI.GetNumOfInteriorExits(_interior); i++)
+            for (int i = 0; i < WorldAPI.GetNumOfInteriorExits(_interior); i++)
             {
-                var x = MWorld.WorldAPI.GetInteriorExit(_interior, i);
+                var x = WorldAPI.GetInteriorExit(_interior, i);
                 _interiorExits.Add(x);
                 MDebug.MDebugAPI.LogDebug($"{i} {JsonConvert.SerializeObject(x)}");
             };
