b40a4b0d477935f489b6397755926208113e4d1f
Wayne-Thornberry <way-net@outlook.com>
2022-08-08 10:42:13 +0100

    removed update.bat

--- code/resources/ProlineCore/Proline.ModualLoader.Loader/update.ps1
+++
@@ -1,5 +0,0 @@
-﻿Remove-Item ".\*"  -Recurse
-Copy-Item -Path "E:\OneDrive\Repo\Project Online\data\resources\ProlineCore\*" -Destination ".\" -Recurse
-Copy-Item -Path "E:\OneDrive\Repo\Project Online\artifacts\ProlineCore\*" -Destination ".\" -Recurse
-Remove-Item ".\CitizenFX.Core.*.dll" -Recurse
-Remove-Item ".\*.pdb" -Recurse
