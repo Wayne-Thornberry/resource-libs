dc264ce781ed32a2f29ed3e04a2fb88d5101ffaa
Wayne-Thornberry <way-net@outlook.com>
2022-11-04 09:58:28 +0000

    updated to local artifacts

--- deploy.ps1
+++ deploy.ps1
@@ -5,9 +5,9 @@ New-Item -Path "D:\ProjectOnline\resources" -Name "client-core" -ItemType "direc
 New-Item -Path "D:\ProjectOnline\resources" -Name "server-core" -ItemType "directory" 
 Copy-Item -Path ".\data\resources\ProlineCore\*" -Destination "D:\ProjectOnline\resources\client-core\" -Recurse
 Copy-Item -Path ".\data\resources\ServerCore\*" -Destination "D:\ProjectOnline\resources\server-core\" -Recurse
-Copy-Item -Path "E:\artifacts\ServerLauncher\*" -Destination "D:\ProjectOnline\" -Recurse
-Copy-Item -Path "E:\artifacts\ProlineCore\*" -Destination "D:\ProjectOnline\resources\client-core\" -Recurse
-Copy-Item -Path "E:\artifacts\ServerCore\*" -Destination "D:\ProjectOnline\resources\server-core\" -Recurse
+Copy-Item -Path ".\artifacts\ProjectOnline*" -Destination "D:\ProjectOnline\" -Recurse
+Copy-Item -Path ".\artifacts\*Client*" -Destination "D:\ProjectOnline\resources\client-core\" -Recurse
+Copy-Item -Path ".\artifacts\*Server*" -Destination "D:\ProjectOnline\resources\server-core\" -Recurse
 Remove-Item "D:\ProjectOnline\resources\client-core\CitizenFX.Core.*.dll" -Recurse
 Remove-Item "D:\ProjectOnline\resources\client-core\*.pdb" -Recurse
 Remove-Item "D:\ProjectOnline\resources\server-core\CitizenFX.Core.*.dll" -Recurse
